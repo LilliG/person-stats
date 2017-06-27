d30453d5d482ab460646a648a1dfe8b3bb58c2e2
Lillian Gray <lmgray16@earlham.edu>
2017-06-27 15:53:57 -0400

    Added stats in an unordered list and displayed color in another div

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 function changeHeading(ev) {
   ev.preventDefault()
   const f = ev.target
-  const text = f.personName.value + ' ' + f.personAge.value
-  //const heading = document.querySelector('h1')
-  //heading.innerHTML = name
-  const stats = document.querySelector('#stats p')
-  stats.innerHTML = text
-
+  const name = f.personName.value
+  const age = f.personAge.value
   const color = f.colorName.value
-  stats.style.color = color
+  
+  const stats = document.querySelector('#stats')
+
+  const list = document.createElement('ul')
+
+  const nameItem = document.createElement('li')
+  nameItem.textContent = `Name: ${name}`
+  list.appendChild(nameItem)
+
+  const ageItem = document.createElement('li')
+  ageItem.textContent = `Age: ${age}`
+  list.appendChild(ageItem)
+
+  const colorItem = document.createElement('li')
+  colorItem.textContent = `Favorite Color:`
+  list.appendChild(colorItem)
+
+  const colorDiv = document.createElement('div')
+  colorDiv.style.backgroundColor = color
+  colorDiv.style.width = '6rem'
+  colorDiv.style.height = "3rem"
+  colorItem.appendChild(colorDiv)
+
+  stats.appendChild(list)
 }
 
 const personForm = document.querySelector('#person-form')
