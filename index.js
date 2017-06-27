60901b04f56d234d270e524d52c528e8f0078dd0
Lillian Gray <lmgray16@earlham.edu>
2017-06-27 14:52:17 -0400

    Added tweaks based on hw review

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function changeHeading(ev) {
   const text = f.personName.value + ' ' + f.personAge.value
   //const heading = document.querySelector('h1')
   //heading.innerHTML = name
-  const stats = document.querySelector('#stats')
+  const stats = document.querySelector('#stats p')
   stats.innerHTML = text
 
   const color = f.colorName.value
